37fe089f39026f821afbee3e6fcf072c16e887da
Zhencheng Chen <42793066+Oeluz@users.noreply.github.com>
2021-10-05 20:48:30 -0400

    Finished the base code, need to add LocalStorage and SessionStorage then work on CSS

--- ProjectOne/data.js
+++ ProjectOne/data.js
@@ -1,69 +1,65 @@
+var depth = 2;
+
 var selectInfo = {
-    "choices" : [
-          {
-            "key": "Main",
-            "options": ["Ferrari", "Lamborghini" , "Masserati"],
-            "description": "Select a Car Brand"
-          },
-          {
-            "key": "Ferrari",
-            "options": ["Enzo", "Italia", "Roma"],
-            "description": "Select a Ferrari Model"
-          },
-          {
-            "key": "Lamborghini",
-            "options": ["Aventador", "Veneno"],
-            "description": "Select a Lamborghini Model"
-          },
-          {
-            "key": "Masserati",
-            "options": ["Ghibli", "Levante", "Quattroporte", "MC20"],
-            "description": "Select a Masserati Model"
-          },
-          {
-            "key": "Enzo",
-            "options": ["Red", "Black"],
-            "description": "Select an Enzo Color"
-          },
-          {
-            "key": "Italia",
-            "options": ["Blue", "Pink"],
-            "description": "Select an Italia Color"
-          },
-          {
-            "key": "Roma",
-            "options": ["Silver", "Red", "White"],
-          "description": "Select a Roma Color"
-          },
-          {
-            "key": "Aventador",
-            "options": ["Green", "Purple"],
-            "description": "Select an Aventador Color"
-          },
-          {
-            "key": "Veneno",
-            "options": ["Orange", "Grey"],
-            "description": "Select a Veneno Color"
-          },
-          {
-            "key": "Ghibli",
-            "options": ["White", "Blue", "Silver"],
-            "description": "Select a Ghibli Color"
-          },
-          {
-            "key": "Levante",
-            "options": ["White", "Black", "Silver"],
-            "description": "Select a Levante Color"
-          },
-          {
-            "key": "Quattroporte",
-            "options": ["White", "Black", "Red"],
-            "description": "Select a Quattroporte Color"
-          },
-          {
-            "key": "MC20",
-            "options": ["Yello", "Black", "Red"],
-            "description": "Select a MC20 Color"
-          }
-        ]
-  }
+  "choices": [{
+      "key": "Main",
+      "options": ["Nintendo Switch", "Xbox One", "PlayStation"],
+      "description": "Select a game console"
+    },
+    {
+      "key": "Nintendo Switch",
+      "options": ["Regular", "Lite", "OLED"],
+      "description": "Select a variety of Nintendo Switch"
+    },
+    {
+      "key": "Xbox One",
+      "options": ["Series S", "Series X"],
+      "description": "Select a variety of Xobox One"
+    },
+    {
+      "key": "PlayStation",
+      "options": ["PS3", "PS4", "PS5"],
+      "description": "Select a generation"
+    },
+    {
+      "key": "Regular",
+      "options": ["Red", "Black", "Yellow", "Blue"],
+      "description": "Select a Nintendo Switch Regular color"
+    },
+    {
+      "key": "Lite",
+      "options": ["Blue", "Red", "Yellow", "Green", "Pink"],
+      "description": "Select a Nintendo Switch Lite color"
+    },
+    {
+      "key": "OLED",
+      "options": ["Blue", "Red", "White", "Gray", "Tan"],
+      "description": "Select a Nintendo Switch OLED color"
+    },
+    {
+      "key": "Series X",
+      "options": ["Green", "Orange", "Yellow", "Blue", "Red"],
+      "description": "Select a Xbox One Series X color"
+    },
+    {
+      "key": "Series S",
+      "options": ["Teal", "Grey", "Black", "Green", "Yellow"],
+      "description": "Select a Xbox One Series S color"
+    },
+    {
+      "key": "PS3",
+      "options": ["Black", "Gray", "Red"],
+      "description": "Select a PS3 Color"
+    },
+    {
+      "key": "PS4",
+      "options": ["Black", "Gray", "Dark Blue", "Red", "Green"],
+      "description": "Select a PS4 Color"
+    },
+    {
+      "key": "PS5",
+      "options": ["White", "Black", "Red", "Red", "Green", "Gray"],
+      "description": "Select a PS5 Color"
+    }
+  ]
+}
--- ProjectOne/javascript.js
+++ ProjectOne/javascript.js
@@ -1,49 +1,84 @@
-step = 0;
+let currentChoice = selectInfo.choices[0];
+var currentKey = currentChoice.key;
+var maxDepth = depth;
+var depth = 0;
+let selections = [];
 
-function createList(currentStep) {
-    const select = document.createElement("select");
-    select.id = "select" + currentStep;
-    container.append(select);
-
-    const option = document.createElement("option");
-    option.value = selectInfo.choices[0].key;
-    option.textContent = "Make a selection";
-    select.append(option);
-
-    selectInfo.choices[0].options.forEach(element => {
-        let option1 = document.createElement("option");
-        option1.value = element;
-        option1.textContent = element;
-        select.append(option1);
-
-        select.addEventListener("change", () => {
-            currentSelection = select.value;
-            console.log(currentSelection);
-
-            selectInfo.choices.forEach(element => {
-                if (element.key == select.value) {
-                    console.log(element);
-                    createSelection(currentStep + 1);
-                }
-            }); //end of loop
-        }); //end of event listener
-    }); //end of loop
-}//end of selectList
-
-function createHeading(currentStep) {
-    const listHeading = document.createElement("h2");
-    listHeading.id = "listHeading";
-    listHeading.textContent = selectInfo.choices[currentStep].description;
-    container.append(listHeading);
-}//end of createHeading
-
-function createSelection(currentStep){
-    createHeading(currentStep);
-    createList(currentStep);
-}
 
 const container = document.createElement("div");
 container.id = "container";
 document.body.append(container);
 
-createSelection(step);
+function getNextChoice(){
+    selectInfo.choices.forEach(element => {
+
+        if(element.key.match(currentKey)){
+            currentChoice = element;
+            depth++;
+            console.log(currentChoice.key);
+        }
+    });
+}
+
+function displayHeading(){
+    const listHeading = document.createElement("h2");
+    listHeading.id = "heading" + depth;
+    listHeading.textContent = currentChoice.description;
+    container.append(listHeading);
+}
+
+function displayList(){
+    const select = document.createElement("select");
+    select.id = "select" + depth;
+    container.append(select);
+    
+    select.addEventListener("change", () => {
+        if(depth == maxDepth)
+        {
+            selections.push(select.value);
+            select.disabled = true;
+            displayResult();
+            return;
+        }
+
+        currentKey = select.value;
+        selections.push(currentKey);
+        getNextChoice();
+        displayCategory();
+        select.disabled = true;
+    })
+
+    addDefaultOption(select);
+    currentChoice.options.forEach(element => {
+        let option = document.createElement("option");
+        option.value = element;
+        option.textContent = element;
+        select.append(option);        
+    });
+
+}
+
+function addDefaultOption(select){
+    let option = document.createElement("option");
+    option.value = "default";
+    option.textContent = "Please make a selection";
+    select.append(option);
+}
+
+function displayResult(){
+    const resultHeading = document.createElement("h3");
+    resultHeading.textContent = "Here is your result!";
+    container.append(resultHeading);
+
+    const resultText = document.createElement("p");
+    resultText.textContent = selections[2] + " " + selections[0] + " " + selections[1];
+    container.append(resultText);
+}
+
+function displayCategory(){
+    displayHeading();
+    displayList();
+}
+
+
+displayCategory();
