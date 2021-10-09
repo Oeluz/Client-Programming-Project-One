ce0a10e17f28d5a67b4ee851c15d96bb9c2936dc
Zhencheng Chen <42793066+Oeluz@users.noreply.github.com>
2021-10-09 12:21:50 -0400

    added comments

--- ProjectOne/javascript.js
+++ ProjectOne/javascript.js
@@ -1,9 +1,13 @@
 let currentChoice = selectInfo.choices[0];
 var currentKey = currentChoice.key;
+
+// get the max depth from the depth property in data.js
 var maxDepth = depth;
+
 var depth = 0;
 let selections = [];
 
+//set up 2 divs
 const head = document.createElement("div");
 head.id = "head";
 document.body.append(head);
@@ -12,10 +16,7 @@ const container = document.createElement("div");
 container.id = "container";
 document.body.append(container);
 
-const footer = document.createElement("div");
-footer.id = "footer";
-document.body.append(footer);
-
+// will be called when the user make a choice in a select
 function getNextChoice() {
     selectInfo.choices.forEach(element => {
 
@@ -27,6 +28,7 @@ function getNextChoice() {
     });
 }
 
+// make a "question" from the description in data.js
 function displayHeading() {
     const listHeading = document.createElement("h2");
     listHeading.id = "heading" + depth;
@@ -34,11 +36,13 @@ function displayHeading() {
     container.append(listHeading);
 }
 
+// make a select with options 
 function displayList() {
     const select = document.createElement("select");
     select.id = "select" + depth;
     container.append(select);
 
+    //check if depth reach the max depth, if so, stop at there and dispaly result
     select.addEventListener("change", () => {
         if (depth == maxDepth) {
             selections.push(select.value);
@@ -64,6 +68,7 @@ function displayList() {
 
 }
 
+// add default option to the select
 function addDefaultOption(select) {
     let option = document.createElement("option");
     option.value = "default";
@@ -71,6 +76,7 @@ function addDefaultOption(select) {
     select.append(option);
 }
 
+// make the messages that display the result of the user's choices
 function displayResult() {
     const resultHeading = document.createElement("h3");
     resultHeading.textContent = "Here is your result!";
@@ -88,11 +94,13 @@ function displayResult() {
     resetBtn.addEventListener("click", reset)
     container.append(resetBtn);
 
+    // save to the sessionStorage with the choices
     for (let i = 0; i <= maxDepth; i++) {
         sessionStorage.setItem("result" + i, document.getElementById("select" + i).value);
     }
 }
 
+// remove the choices and result then start over
 function reset() {
     for (let i = 0; i <= maxDepth; i++) {
         sessionStorage.removeItem("result" + i);
@@ -110,11 +118,13 @@ function reset() {
     displayCategory();
 }
 
+// display one heading + one select + multiple options
 function displayCategory() {
     displayHeading();
     displayList();
 }
 
+// if the localStorage don't have username, the user is a guest
 function displayHeadGuest() {
     const form = document.createElement("form");
     form.id = "loginForm";
@@ -138,6 +148,7 @@ function displayHeadGuest() {
 
     head.append(form);
 
+    //add the input to username in localStorage
     form.addEventListener("submit", (e) => {
         e.preventDefault();
         localStorage.setItem("username", form.username.value.trim());
@@ -145,6 +156,7 @@ function displayHeadGuest() {
     });
 }
 
+// if localStorage have username, the user is an old user
 function displayHeadUser() {
     const welcomeMsg = document.createElement("h2");
     welcomeMsg.id = "welcome-message"
@@ -158,11 +170,13 @@ function displayHeadUser() {
     head.append(logoutBtn);
 }
 
+// remove the username from localStorage
 function logout(event) {
     localStorage.removeItem("username");
     checkLocalStorage();
 }
 
+// display guest or user info depends on the local storage
 function checkLocalStorage() {
 
     if (localStorage.getItem("username")) {
@@ -179,6 +193,7 @@ function checkLocalStorage() {
     }
 }
 
+// check the local storage and session storage to show what info
 function onStart() {
     if (localStorage.getItem("username")) {
         displayHeadUser();
@@ -203,6 +218,12 @@ function onStart() {
             select.append(option);
             select.disabled = true;
         }
+
+        selections[0] = sessionStorage.getItem("result0");
+        selections[1] = sessionStorage.getItem("result1");
+        selections[2] = sessionStorage.getItem("result2");
+        displayResult();
+
     }
     else {
         displayCategory();
